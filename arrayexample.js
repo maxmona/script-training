61d3169f1c0dd26b0c8ad57688052570b907dcc1
Shaheed <irfan.shaheed@capgemini.com>
2018-03-10 11:43:13 +0530

    adding JS array with different type of for loops

---
+++ arrayexample.js
@@ -0,0 +1,18 @@
+
+var numList=[10,20,30,40,50,60,70,80,90,100];
+var list=[10,15,"heyyy",12.50];
+
+// For loop
+for(i=0;i<numList.length;i++){
+    console.log(numList[i])
+}
+
+//for in loop
+
+for (let a in numList){
+    console.log(numList[a])
+    }
+//for each loop    
+numList.forEach(element=>{
+    console.log(element)
+    });
